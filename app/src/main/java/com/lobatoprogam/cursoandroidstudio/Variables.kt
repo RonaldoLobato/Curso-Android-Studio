1e30216e79e66b644e74827e8418d712a5e69f21
fullstackprog <ronaldLobato27@outlook.com>
2023-05-22 02:41:02 -0500

    Valores y variables en Kotlin

---
+++ app/src/main/java/com/lobatoprogam/cursoandroidstudio/Variables.kt
@@ -0,0 +1,61 @@
+package com.lobatoprogam.cursoandroidstudio
+
+/*
+* Variables
+* Var : Es una variable -> Su valor se puede modificar
+* Val : Es una constante -> Su valor no se puede modifcar
+*/
+
+fun main(){
+
+    /*
+    * Variables numericas
+    */
+
+    //Int -> Soporta -2,147,483,467 a 2,147,483,467
+    val intExample:Int = 20
+
+    //Long -> Soporta -9,147,483,467,147,483,467 a 2,147,483,467,147,483,467
+    val longExample:Long = 30
+
+    //Float -> Soporta
+    val floatExample:Float = 30.5f
+
+    //Double -> Soporta ->
+    val doubleExample:Double = 487.52
+
+    /*
+    * Variables Alfanumericas (Cadena)
+    */
+
+    //Char -> Soporta un caracter
+    val charExample1:Char = '1'
+    val charExample2:Char = 'M'
+    val charExample3:Char = '_'
+
+    //String -> Soporta infinito de caracteres
+    val stringExample = "Ronaldo Lobato, es un programador"
+
+    /*
+    * Variables Booleanas
+    */
+    val booleanExample1:Boolean = true
+    val booleanExample2:Boolean = false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
